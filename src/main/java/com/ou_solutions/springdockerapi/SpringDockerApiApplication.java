33f0d81b06d15d849189aaf6be9b2e051b7b3238
Omkar Upadhyay <omkarmh009@gmail.com>
2023-10-28 14:12:52 +0800

    first commit

---
+++ src/main/java/com/ou_solutions/springdockerapi/SpringDockerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.springdockerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ou_solutions/springdockerapi/SpringDockerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.springdockerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
